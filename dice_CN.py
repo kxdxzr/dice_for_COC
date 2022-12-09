5c51ec0e593155f375ba4351cc06cf5f37b2ffcf
Steve Yu <74366156+kxdxzr@users.noreply.github.com>
2022-12-09 18:44:02 +1100

    Add files via upload

--- dice_CN.py
+++ dice_CN.py
@@ -42,11 +42,13 @@ def random(value):
     result = randint(1, value)
     result_str = str(result).zfill(3)
     try:
+        settings[1] = values["Fumbles"].zfill(2)
         Fumbles_value = int(values["Fumbles"])
     except:
         result_window.update(value = "大失败错误", text_color = "Red")
         return
     try:
+        settings[2] = values["Criticals"].zfill(2)
         Criticals_value = int(values["Criticals"])
     except:
         result_window.update(value = "大成功错误", text_color = "Red")
@@ -76,6 +78,20 @@ def Main_GUI():
     sg_theme = sg.theme("Default1")
     global log_list
     log_list = []
+    global settings
+    try:
+        settings = load_from_log('settings.set')
+    except:
+        settings = ["中文","96","05"]
+    
+    ############# initial value #############
+    
+    ############# Two languages #############
+    skill_name_text = ["技能名", "Skill Name"]
+    PC_name_text = ["PC名", "PC Name"]
+    
+    
+    ############# Two languages #############
     
     ############## log control ###############
     global Showing
@@ -88,12 +104,12 @@ def Main_GUI():
     
     ############# initial value #############
     Column1 = sg.Column([
-        [sg.Text("技能名",font = ("Times New Roman",12))],
+        [sg.Text("技能名",font = ("Times New Roman",12),key = "skill_name_key")],
         [sg.Input(default_text = "",
                   size = (10, 2), 
                   key=("Skill"),
                   enable_events = True),],
-        [sg.Text("PC名",font = ("Times New Roman",12))],
+        [sg.Text("PC名",font = ("Times New Roman",12),key = "PC_name_key")],
         [sg.Input(default_text = "",
                   size = (10, 2), 
                   key=("PC"),
@@ -107,25 +123,25 @@ def Main_GUI():
         [sg.Button('自定义计算', key='Cal_Manual',size = (10,2))],
         ])
     Column2 = sg.Column([
-        [sg.Text("结果",font = ("Times New Roman",12))],
+        [sg.Text("结果",font = ("Times New Roman",12), key = "result_key")],
         [sg.Text("000",key = "Result_Text",
                  font = ("Times New Roman",40))],
-        [sg.Text("日志",font = ("Times New Roman",12))],
+        [sg.Text("日志",font = ("Times New Roman",12),key = "log_key")],
         [sg.Listbox([],size = (40, 15),key = "Log")],
-        [sg.Text("自定义骰子",font = ("Times New Roman",12))],
+        [sg.Text("自定义骰子",font = ("Times New Roman",12), key = "customized_dice_key")],
         [sg.Input(default_text = "",
                   size = (40, 8), 
                   key=("Manual"),
                   enable_events = True),]
         ])
     Column3 = sg.Column([
-        [sg.Text("大失败",font = ("Times New Roman",12))],
-        [sg.Input(default_text = "96",
+        [sg.Text("大失败",font = ("Times New Roman",12), key = "Fumbles_key")],
+        [sg.Input(default_text = settings[1],
                   size = (4, 2), 
                   key=("Fumbles"),
                   enable_events = True)],
-        [sg.Text("大成功",font = ("Times New Roman",12))],
-        [sg.Input(default_text = "05",
+        [sg.Text("大成功",font = ("Times New Roman",12), key = "Criticals_key")],
+        [sg.Input(default_text = settings[2],
                   size = (4, 2), 
                   key=("Criticals"),
                   enable_events = True)],
@@ -135,7 +151,14 @@ def Main_GUI():
                  enable_events=True,
                  key = "Choose_show",
                  initial_value = "Show")],
-        [sg.Button('加载日志', key='load',size = (10,2))]
+        [sg.Button('移除上一条', key='delete',size = (10,2))],
+        [sg.Button('加载日志', key='load',size = (10,2))],
+        [sg.Text("语言/Language",font = ("Times New Roman",12))],
+        [sg.Spin(["中文","EN"],
+                 size = (10,30),
+                 enable_events=True,
+                 key = "language",
+                 initial_value = settings[0])],
         ])
     layout = [
         [Column1,Column2,Column3],
@@ -146,9 +169,8 @@ def Main_GUI():
         [sg.Text("1d???: ? ?  000",key = "Showing",
              font = ("Times New Roman",80))]
         ]
-    
-    window = sg.Window("Dice", layout, finalize=True,resizable=True)
     window2 = sg.Window("Front_end", layout2, finalize=True,resizable=True)
+    window = sg.Window("Dice", layout, finalize=True,resizable=True)
     ############# layout #############
     global result_window
     result_window = window["Result_Text"]
@@ -166,6 +188,7 @@ def Main_GUI():
         event, values = window.read(timeout=0.1)
         if event in ['EXIT_BUTTON', sg.WIN_CLOSED]:
             save_to_txt(log_list,"log.log")
+            save_to_txt(settings,"settings.set")
             window.close()
             window2.close()
             return
@@ -216,6 +239,15 @@ def Main_GUI():
                 log_window.update(values = log_list, scroll_to_index = len(log_list))
             except:
                 result_window.update(value = "无日志文件", text_color = "Red")
+        elif event == 'delete':
+            try:
+                log_list.pop()
+                log_window.update(values = log_list, scroll_to_index = len(log_list))
+                result_window.update(value = "000", text_color = "Black")
+            except:
+                pass
+        elif event == 'language':
+            pass
         else:
             pass
         ##################### GUI Handling #####################
